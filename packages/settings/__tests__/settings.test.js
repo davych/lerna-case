18a59396b596e60e1fde83b0613951e47d73aad2
davy_chen <davy_chen@epam.com>
2022-10-22 14:33:01 +0800

    -

--- packages/settings/__tests__/settings.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const settings = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(settings(), 'Hello from settings');
-console.info("settings tests passed");
--- packages/settings/lib/settings.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = settings;
-
-function settings() {
-    return "Hello from settings";
-}
